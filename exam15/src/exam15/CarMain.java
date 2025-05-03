21803a4f4bffa03137fae7fe05dc8a888acfb5ad
jihoonjeong56 <jeong283056@gmail.com>
2025-05-03 17:05:19 +0900

    new file

---
+++ exam15/src/exam15/CarMain.java
@@ -0,0 +1,15 @@
+package exam15;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		car.year = 2020;
+		car.carName = "쏘나타";
+		car.go();
+		 
+
+	}
+
+}
