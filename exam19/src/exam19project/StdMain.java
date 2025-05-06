63eadeabe5d3641e26a4e5fe78251bd773e29942
jihoonjeong56 <jeong283056@gmail.com>
2025-05-06 16:19:08 +0900

    new file

---
+++ exam19/src/exam19project/StdMain.java
@@ -0,0 +1,15 @@
+package exam19project;
+
+public class StdMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StdService service = new StdService();
+		//학생생성
+		service.createStudent();
+		//출력5
+		service.printStudnet();
+	}
+
+}
